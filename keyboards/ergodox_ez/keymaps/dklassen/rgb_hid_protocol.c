8968344241f6c0e248d4b2c35d767aaba0accac2
Dana Klassen <dana.klassen@shopify.com>
2019-02-28 21:13:09 -0500

    Add additional commands

--- keyboards/ergodox_ez/keymaps/dklassen/rgb_hid_protocol.c
+++ keyboards/ergodox_ez/keymaps/dklassen/rgb_hid_protocol.c
@@ -7,7 +7,16 @@ enum hid_commands
   hid_version = 0x01,
   hid_rgblight_enable,
   hid_rgblight_disable,
-  hid_rgblight_toggle 
+  hid_rgblight_toggle,
+  hid_rgblight_set_mode,
+  hid_rgblight_get_mode,
+  hid_rgblight_sethsv,
+  hid_rgblight_increase_val,
+  hid_rgblight_decrease_val,
+  hid_rgblight_increase_sat,
+  hid_rgblight_decrease_sat,
+  hid_rgblight_increase_hue,
+  hid_rgblight_decrease_hue
 };
 
 
@@ -27,6 +36,38 @@ void raw_hid_receive( uint8_t *data, uint8_t length ) {
     case hid_rgblight_toggle:
 	    rgblight_toggle();
 	    break;
+	  case hid_rgblight_set_mode:
+	    rgblight_mode(data[1]);
+	    break;
+	  case hid_rgblight_get_mode:
+	    data[1] = rgblight_get_mode();
+	    break;
+	  case hid_rgblight_sethsv:
+      {
+        // Hue is 0 - 360, uint8_t is 0 - 255
+        // so we split into two entries.
+	      uint16_t hue = (data[1] << 8) + data[2]; 
+	      rgblight_sethsv(hue, data[3], data[4]);
+        break;
+      }
+    case hid_rgblight_increase_val:
+      rgblight_increase_val();
+      break;
+    case hid_rgblight_decrease_val:
+      rgblight_decrease_val();
+      break;
+    case hid_rgblight_increase_sat:
+      rgblight_increase_sat();
+      break;
+    case hid_rgblight_decrease_sat:
+      rgblight_decrease_sat();
+      break;
+    case hid_rgblight_increase_hue:
+      rgblight_increase_hue();
+      break;
+    case hid_rgblight_decrease_hue:
+      rgblight_increase_hue();
+      break;
 	  default:
 	    data[0] = 0xFF;
 	    break;
